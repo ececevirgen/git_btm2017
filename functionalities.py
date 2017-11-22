4f85877b6e706d3150f62ae10857bdd2fd59cf1e
=ece <=>
2017-11-22 15:31:17 +0000

    client entry

--- functionalities.py
+++ functionalities.py
@@ -4,7 +4,12 @@ def say_hi():
     print("Welcome to potato bank, how can I help you?")
 
 def new_client():
-    pass
+    new_client = input("...New client name: ")
+    if not new_client in fM.get_clients():
+        fM.add_client(new_client)
+        print("Client added !")
+    else:
+        print("Client already exist !")
     
 def new_transaction():
     pass
